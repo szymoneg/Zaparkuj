a901fbac80d65e267bb996611d40acd2ffdd0a41
Szymon <szymon.zsp5@gmail.com>
2020-12-04 21:58:16 +0100

    +registration validation

---
+++ app/src/main/java/com/example/parkIt/Regexes.kt
@@ -0,0 +1,17 @@
+package com.example.parkIt
+
+import android.util.Log
+
+class Regexes {
+    private val mailPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\\.[a-zA-Z.]{2,18}".toRegex()
+    private val passwordPattern = "[a-zA-Z0-9?.*@\$!%#&]{8,32}".toRegex() //znaki dostepne ?.*@$!%#&
+
+
+    fun checkMail(mail: String): Boolean {
+        return mailPattern.matches(mail);
+    }
+
+    fun checkPassword(password: String): Boolean{
+        return passwordPattern.matches(password);
+    }
+}
--- app/src/main/java/com/example/parkIt/RegisterActivity.kt
+++ app/src/main/java/com/example/parkIt/RegisterActivity.kt
@@ -17,6 +17,7 @@ import java.io.IOException
 class RegisterActivity : AppCompatActivity() {
     private lateinit var email: EditText
     private lateinit var password: EditText
+    private lateinit var password2: EditText
     private val mediaType = "application/json; charset=utf-8".toMediaType()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +25,7 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
         email = findViewById(R.id.editTextRegEmail)
         password = findViewById(R.id.editTextRegPassword)
+        password2 = findViewById(R.id.editTextRegPassword2)
 
         val button = findViewById<Button>(R.id.buttonCreate)
         button.setOnClickListener {
@@ -31,34 +33,57 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkRegValues(): Boolean {
+        val reg = Regexes()
+        if (reg.checkMail(email.text.toString())) {
+            return if (password.text.toString() == password2.text.toString()) {
+                if (reg.checkPassword(password.text.toString())) {
+                    true;
+                } else {
+                    password.error = "Password doesn't meet the requirements: Length: 8-32. Allowed special chars: ?.*@\\$!%#&"
+                    false;
+                }
+            } else {
+                password2.error = "Passwords don't match";
+                false;
+            }
+        } else {
+            email.error = "Incorrect email!"
+            return false;
+        }
+    }
+
     private fun sendMessage() {
-        val email = email.text
-        val password = password.text
-        val url = "http://10.0.2.2:8080/register"
-        val client = OkHttpClient()
-        val rootObject = JSONObject()
-        rootObject.put("username", "Test")
-        rootObject.put("password", password.toString())
-        rootObject.put("email", email.toString())
+        if (checkRegValues()) {
+            val email = email.text
+            val password = password.text
+            val url = "http://10.0.2.2:8080/register"
+            val client = OkHttpClient()
+            val rootObject = JSONObject()
+            rootObject.put("username", "Test")
+            rootObject.put("password", password.toString())
+            rootObject.put("email", email.toString())
 
-        val body = rootObject.toString().toRequestBody(mediaType)
+            val body = rootObject.toString().toRequestBody(mediaType)
 
-        val request = Request.Builder()
-                .url(url)
-                .post(body)
-                .build()
+            val request = Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build()
 
-        client.newCall(request).enqueue(object : Callback {
-            override fun onResponse(call: Call, response: Response) {
-                Log.i("Response code: ",response.code.toString())
-                hideKey()
-            }
+            client.newCall(request).enqueue(object : Callback {
+                override fun onResponse(call: Call, response: Response) {
+                    Log.i("Response code: ", response.code.toString())
+                    hideKey()
+                }
 
-            override fun onFailure(call: Call, e: IOException) {
-                print(e.printStackTrace())
-            }
-        })
-
+                override fun onFailure(call: Call, e: IOException) {
+                    print(e.printStackTrace())
+                }
+            })
+        } else {
+            Log.e("---Regex", "Wrong values")
+        }
     }
 
     private fun hideKey() {
