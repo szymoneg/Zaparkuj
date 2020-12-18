673631f9b53138cf4c8928f040abd997839f4424
Szymon <szymon.zsp5@gmail.com>
2020-12-19 00:05:04 +0100

    TODO sharedpreferences

--- app/src/main/java/com/example/parkIt/LoginActivity.kt
+++ app/src/main/java/com/example/parkIt/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.example.parkIt
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -18,6 +20,12 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var email: EditText;
     private lateinit var password: EditText;
     private val mediaType = "application/json; charset=utf-8".toMediaType()
+    private lateinit var token: String;
+
+
+    //TODO sharedpreferences
+    private val PREF_NAME = "jwtToken"
+    val sharedPreferences: SharedPreferences = getSharedPreferences(PREF_NAME,Context.MODE_PRIVATE)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +37,31 @@ class LoginActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.buttonLogin)
         button.setOnClickListener {
             loggin()
+            Thread.sleep(5000)
+            run()
         }
+    }
+
+    fun run() {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("http://10.0.2.2:8080/hello")
+            .addHeader("Authorization", "Bearer $token")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    Log.i("Value","XDDD");
+                }
+            }
+        })
     }
 
     private fun loggin() {
@@ -54,6 +86,7 @@ class LoginActivity : AppCompatActivity() {
                 val json = Gson()
                 val value = json.fromJson(response.body?.string(),JwtTokenData::class.java)
                 //TODO zapisywanie value do sesji!
+                token = value.jwttoken;
                 Log.i("Response code: ", value.jwttoken)
             }
 
