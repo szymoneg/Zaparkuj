29ad5f36ad88a484873799ddb3ea6a81323c8d5b
unknown <andrzejosika06@gmail.com>
2020-12-29 18:24:05 +0100

    +username in register, changed email to username in login

--- app/src/main/java/com/example/parkIt/LoginActivity.kt
+++ app/src/main/java/com/example/parkIt/LoginActivity.kt
@@ -2,10 +2,7 @@ package com.example.parkIt
 
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -13,14 +10,11 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import com.google.gson.Gson
-import com.google.gson.JsonParser
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
-import org.w3c.dom.Text
 import java.io.IOException
 
 
@@ -35,7 +29,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        email = findViewById(R.id.editTextLogEmail)
+        email = findViewById(R.id.editTextLogUser)
         password = findViewById(R.id.editTextLogPassword)
         erorrText = findViewById(R.id.textViewErorr)
 
