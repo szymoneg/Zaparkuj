0270bcc2024fb59e741dd63b164aec0875041ee8
kaburen <andrzejosika06@gmail.com>
2021-01-06 04:59:29 +0100

    +spots recycler, fixed date and time selector

---
+++ app/src/main/java/com/example/parkIt/SelectSpotActivity.kt
@@ -0,0 +1,11 @@
+package com.example.parkIt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectSpotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_spot)
+    }
+}
---
+++ app/src/main/java/com/example/parkIt/data/SpotItem.kt
@@ -0,0 +1,3 @@
+package com.example.parkIt.data
+
+data class SpotItem()
